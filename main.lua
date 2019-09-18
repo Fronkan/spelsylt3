406ac3189a5573ef034beff516029671d0d217cb
Fredrik Bengtsson <fredrik.bengtsson94@gmail.com>
2019-09-18 21:55:38 +0200

    make camera follow player and add test for HC collision lib

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 lurker = require("external.lurker")
 Player = require("player")
-Bump = require("external.bump_lib.bump")
 Enemy = require("enemy")
+HC = require("external.HC")
 local HumpCamera = require("external/hump/camera")
 
 local scale = 1
@@ -12,15 +12,24 @@ local enemy = Enemy(-200, 100, player)
 
 function love.load()
     GAME_STATE = {
-        PYSICS_WORLD = ""
+        PYSICS_WORLD = HC
     }
     camera = HumpCamera(0,0, scale, 0)
+
+    -- HC test½
+    rect = HC.rectangle(0,0,400,20)
+    -- END HC test
 end
 
 function love.update(dt)
+    -- HC test
+    rect:rotate(dt)
+    -- END HC test
     lurker.update()
     player:update(dt)
     enemy:update(dt)
+    local dx,dy = player.pos.x - camera.x, player.pos.y - camera.y
+    camera:move(dx/2, dy/2)
 end
 
 -- Just test to print to the graphics window and the terminal
@@ -35,5 +44,9 @@ function love.draw()
     end
     player:draw()
     enemy:draw()
+    -- HC test
+    love.graphics.setColor(255,255,255)
+    rect:draw('fill')
+    -- END HC test
     camera:detach()
 end
